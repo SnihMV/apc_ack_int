cf1424caf05bd8285c81deb3da5fef3f8b5fbda4
m.snihovskiy <m.snihovskiy@petrosoft.su>
2025-11-17 19:49:25 +0300

    add CORS

--- src/main/java/su/petrosoft/apk_ack_integration/config/GlobalCorsConfig.java
+++ src/main/java/su/petrosoft/apk_ack_integration/config/GlobalCorsConfig.java
@@ -1,11 +1,13 @@
 package su.petrosoft.apk_ack_integration.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@Slf4j
 @Configuration
 public class GlobalCorsConfig {
 
@@ -15,6 +17,7 @@ public class GlobalCorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("https://plicante-apk-test-vue.plicante.ru");
         config.addAllowedOriginPattern("http://192.168.0.*:[*]");
         config.addAllowedOriginPattern("http://localhost:[*]");
         config.addAllowedHeader("*");
