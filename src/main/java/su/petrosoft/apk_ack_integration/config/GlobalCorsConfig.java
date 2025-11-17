c1c6a443f911771af166ebf9e8351b1e80987d83
m.snihovskiy <m.snihovskiy@petrosoft.su>
2025-11-17 19:37:14 +0300

    add CORS

---
+++ src/main/java/su/petrosoft/apk_ack_integration/config/GlobalCorsConfig.java
@@ -0,0 +1,30 @@
+package su.petrosoft.apk_ack_integration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("http://192.168.0.*:[*]");
+        config.addAllowedOriginPattern("http://localhost:[*]");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedMethod("DELETE");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
